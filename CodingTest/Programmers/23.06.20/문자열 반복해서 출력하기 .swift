669df574e3c96a8d05f100c9890a8149d3304650
Jieun <0330jieun@gmail.com>
2023-06-21 10:16:20 +0900

    [Programmers] 문자열 반복해서 출력하기

---
+++ CodingTest/Programmers/23.06.20/문자열 반복해서 출력하기 .swift
@@ -0,0 +1,8 @@
+//
+//  문자열 반복해서 출력하기 .swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/06/20.
+//
+
+import Foundation
---
+++ CodingTest/Programmers/23.06.21/문자열 반복해서 출력하기.swift
@@ -0,0 +1,14 @@
+//
+//  문자열 반복해서 출력하기.swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/06/21.
+//
+
+import Foundation
+
+let inp = readLine()!.components(separatedBy: [" "]).map { $0 }
+let (s1, a) = (inp[0], Int(inp[1])!)
+
+let answer = String(repeating: "s1", count: a))
+print(answer)
