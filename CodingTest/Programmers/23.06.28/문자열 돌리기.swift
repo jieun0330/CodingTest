b84f8e3b48927b804aed63819a100612fb05d7c8
Jieun <jieun_0330@naver.com>
2023-06-29 10:56:37 +0900

    [Programmers] 문자열 돌리기

---
+++ CodingTest/Programmers/23.06.28/문자열 돌리기.swift
@@ -0,0 +1,8 @@
+//
+//  문자열 돌리기.swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/06/28.
+//
+
+import Foundation
---
+++ CodingTest/Programmers/23.06.29/문자열 돌리기.swift
@@ -0,0 +1,14 @@
+//
+//  문자열 돌리기.swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/06/29.
+//
+
+import Foundation
+
+let s1 = readLine()!
+
+for i in s1 {
+    print(i)
+}
