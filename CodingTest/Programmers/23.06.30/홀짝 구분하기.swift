6a2b6b517e3299e13e042d4ac92fbb6ec035f4f9
Jieun <jieun_0330@naver.com>
2023-06-30 10:00:14 +0900

    [Programmers] 홀짝 구분하기

---
+++ CodingTest/Programmers/23.06.30/홀짝 구분하기.swift
@@ -0,0 +1,16 @@
+//
+//  홀짝 구분하기.swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/06/30.
+//
+
+import Foundation
+
+let a = Int(readLine()!)!
+
+if a % 2 == 0 {
+    print("\(a) is even")
+}else {
+    print("\(a) is odd")
+}
