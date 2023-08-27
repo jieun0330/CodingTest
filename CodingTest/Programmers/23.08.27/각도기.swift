c8a038f27aaffa80b0e399ea31003236927f88b8
jieun0330 <42729069+jieun0330@users.noreply.github.com>
2023-08-27 23:06:35 +0900

    [Programmers] 각도기

---
+++ CodingTest/Programmers/23.08.27/각도기.swift
@@ -0,0 +1,29 @@
+//
+//  각도기.swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/08/27.
+//
+
+
+import Foundation
+
+//func solution(_ angle:Int) -> Int {
+//    
+//    var result = 0
+//    
+//    switch angle {
+//    case 0..<90:
+//        result = 1
+//    case 90:
+//        result = 2
+//    case 90..<180:
+//        result = 3
+//    case 180:
+//        result = 4
+//    default:
+//        print("에러")
+//    }
+//
+//    return result
+//}
