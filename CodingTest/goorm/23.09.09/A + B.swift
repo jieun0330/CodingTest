b65f4d9d5070ee62b5964ed227d393f2a86c8935
jieun <0330jieun@gmail.com>
2023-09-10 16:10:44 +0900

    [goorm] A + B

---
+++ CodingTest/goorm/23.09.09/A + B.swift
@@ -0,0 +1,19 @@
+//
+//  A + B.swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/09/09.
+//
+
+import Foundation
+
+
+
+let input = readLine()!.components(separatedBy: [" "])
+
+let num1 = Int(input[0])!
+let num2 = Int(input[1])!
+var sum: Int = num1 + num2
+
+
+print(sum)
