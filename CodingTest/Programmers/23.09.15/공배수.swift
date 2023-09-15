31af94b72bad53c5e251e29c6ff896124e645457
jieun <jieun_0330@naver.com>
2023-09-15 13:14:34 +0900

    [Programmers] 공배수

---
+++ CodingTest/Programmers/23.09.15/공배수.swift
@@ -0,0 +1,24 @@
+//
+//  공배수.swift
+//  CodingTest
+//
+//  Created by 박지은 on 2023/09/14.
+//
+
+import Foundation
+
+//func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
+//    
+//    var result = 0
+//    
+//    if number % n == 0 && number % m == 0 {
+//        result = 1
+//        
+//    } else {
+//        result = 0
+//    }
+//    
+//    return result
+//}
+//
+//print(solution(55, 10, 5))
