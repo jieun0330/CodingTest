aa844f826b6622d68981f5a624be616fd49cd485
jieun <jieun_0330@naver.com>
2023-11-22 21:12:25 +0900

    [Baekjoon] 1998년생인 내가 태국에서는 2541년생?!

---
+++ CodingTest/Baekjoon/23.11.22 solution18108.swift
@@ -0,0 +1,15 @@
+//
+//  23.11.22 solution18108.swift
+//  CodingTest
+//
+//  Created by 박지은 on 11/22/23.
+//
+
+import Foundation
+
+func solution18108() {
+    
+    let input = Int(readLine()!)!
+    
+    print(input - 543)
+}
