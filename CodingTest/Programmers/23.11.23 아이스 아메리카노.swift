cf6537135e62904fe9d49c380aad7c7f73932b3b
jieun <jieun_0330@naver.com>
2023-11-23 14:05:49 +0900

    [Programmers] 아이스 아메리카노

---
+++ CodingTest/Programmers/23.11.23 아이스 아메리카노.swift
@@ -0,0 +1,21 @@
+//
+//  23.11.23 아이스 아메리카노.swift
+//  CodingTest
+//
+//  Created by 박지은 on 11/23/23.
+//
+
+import Foundation
+
+//func solution120819(_ money:Int) -> [Int] {
+//    
+//    let iceAme = 5500
+//    
+//    return [money / iceAme, money % iceAme]
+//    
+//}
+
+
+func solution120819(_ money: Int) -> [Int] { [money / 5500, money % 5500]
+    
+}
