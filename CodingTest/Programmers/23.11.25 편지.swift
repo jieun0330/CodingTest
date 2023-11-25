9df14510b921a013dc059cf59f95b81bdf805bac
Jieun <jieun_0330@naver.com>
2023-11-25 16:54:07 +0900

    [Programmers] 편지

---
+++ CodingTest/Programmers/23.11.25 편지.swift
@@ -0,0 +1,12 @@
+//
+//  23.11.25 편지.swift
+//  CodingTest
+//
+//  Created by 박지은 on 11/25/23.
+//
+
+import Foundation
+
+func solution120898(_ message:String) -> Int {
+    return (message.count) * 2
+}
