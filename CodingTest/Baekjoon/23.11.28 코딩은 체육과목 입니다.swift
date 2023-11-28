cb4945fdb52c086524aa7ec19729bd08955f82ff
jieun <jieun_0330@naver.com>
2023-11-29 00:40:50 +0900

    [Baekjoon] 코딩은 체육과목 입니다

---
+++ CodingTest/Baekjoon/23.11.28 코딩은 체육과목 입니다.swift
@@ -0,0 +1,17 @@
+//
+//  23.11.28 코딩은 체육과목 입니다.swift
+//  CodingTest
+//
+//  Created by 박지은 on 11/29/23.
+//
+
+import Foundation
+
+func solution25324() {
+    let input = Int(readLine()!)!/4
+    
+    for _ in 0..<input {
+        print("long", terminator: " ")
+    }
+    print("int")
+}
