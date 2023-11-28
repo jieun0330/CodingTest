2b3892a7670ad94080960fdddc08f2a4c68f8f10
jieun <jieun_0330@naver.com>
2023-11-29 01:27:06 +0900

    [Programmers] 배열 원소의 길이

---
+++ CodingTest/Programmers/2023.11/23.11.29 배열 원소의 길이.swift
@@ -0,0 +1,14 @@
+//
+//  23.11.29 배열 원소의 길이.swift
+//  CodingTest
+//
+//  Created by 박지은 on 11/29/23.
+//
+
+import Foundation
+
+func solution120854(_ strlist:[String]) -> [Int] {
+    strlist.map{ $0.count }
+}
+
+
